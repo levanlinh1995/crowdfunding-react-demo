d4c22fb4d7344346f05268a1a3cfb0c4f82cdd62
Robert <linh.le@gumiviet.com>
2022-10-21 18:48:48 +0700

    update

--- truffle/migrations/1_deploy_factory.js
+++ truffle/migrations/1_deploy_factory.js
@@ -2,4 +2,12 @@ const CampaignFactory = artifacts.require('CampaignFactory');
 
 module.exports = async function (deployer) {
     await deployer.deploy(CampaignFactory);
+
+    const instance = await CampaignFactory.deployed();
+
+    const accounts = await web3.eth.getAccounts();
+
+    await web3.eth.sendTransaction({to: instance.address, from: accounts[0], value: web3.utils.toWei('1', 'ether')});
+
+    // await deployer.deploy(Campaign, '1000', accounts[0]);
 }
